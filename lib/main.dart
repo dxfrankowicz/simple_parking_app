9d2b4502a10b663e1f99a105472d786b8c1e2bbd
Damianox <damianox3@gmail.com>
2021-11-09 15:48:58 +0100

    Location utils added: get current location on init and on button tap. Added title bar

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:simple_parking_app/utils/location_utils.dart';
 
 import 'generated/l10n.dart';
 
@@ -33,10 +34,32 @@ class MapWidget extends StatefulWidget {
 class MapWidgetState extends State<MapWidget> {
   Completer<GoogleMapController> _controller = Completer();
 
+  bool isWaitingForLocalization = false;
+
+  CameraPosition? initialCameraPosition;
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
+
+  @override
+  void initState() {
+    getLocationAndInit();
+    super.initState();
+  }
+
+  Future<void> getLocationAndInit() async {
+    isWaitingForLocalization = true;
+    var location = await LocationUtils.determinePosition();
+    if (location != null)
+      initialCameraPosition = CameraPosition(
+        target: LatLng(location.latitude!, location.longitude!),
+        zoom: 14.4746,
+      );
+    else
+      initialCameraPosition = _kGooglePlex;
+    _goToTheLocation(initialCameraPosition!);
+  }
 
   static final CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
@@ -47,24 +70,53 @@ class MapWidgetState extends State<MapWidget> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: AutoSizeText(S.current.addParkingLocation, maxLines: 1),
-        icon: Icon(Icons.add_location_alt_outlined),
-      ),
-    );
+        appBar: AppBar(
+          centerTitle: true,
+          title: Flexible(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: AutoSizeText(
+                "Simple parking app",
+              ),
+            ),
+          ),
+        ),
+        body: GoogleMap(
+          mapType: MapType.hybrid,
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+        floatingActionButton: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: FloatingActionButton.extended(
+                onPressed: _goToTheLake,
+                label: AutoSizeText(S.current.addParkingLocation, maxLines: 1),
+                icon: Icon(Icons.add_location_alt_outlined),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: FloatingActionButton(
+                backgroundColor: Colors.white,
+                child: Icon(Icons.my_location_rounded, color: Colors.black),
+                onPressed: () => getLocationAndInit(),
+              ),
+            ),
+          ],
+        ));
   }
 
   Future<void> _goToTheLake() async {
     final GoogleMapController controller = await _controller.future;
     controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+
+  Future<void> _goToTheLocation(CameraPosition cameraPosition) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
   }
 }
